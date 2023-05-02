825c15d0bf389199a1dfbbe5d3f3858ab2ff4aeb
NikhilGithubOfficial <nkmnikhil0009@gmail.com>
2023-05-03 01:32:51 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const { exec } = require("child_process");
+
+const words = [
+  "apple",
+  "banana",
+  "cherry",
+  "dog",
+  "elephant",
+  "frog",
+  "giraffe",
+  "house",
+  "ice cream",
+  "jungle",
+  "koala",
+  "lemon",
+  "mango",
+  "ninja",
+  "octopus",
+  "penguin",
+  "queen",
+  "rabbit",
+  "sunflower",
+  "tiger",
+  "umbrella",
+  "volcano",
+  "waterfall",
+  "xylophone",
+  "yacht",
+  "zebra",
+  "astronomy",
+  "beauty",
+  "chemistry",
+  "drama",
+  "economics",
+  "fashion",
+  "geography",
+  "history",
+  "internet",
+  "jazz",
+  "kangaroo",
+];
+
+words.forEach((word) => {
+  exec(`start microsoft-edge:https://www.bing.com/search?q=${word}`);
+});
